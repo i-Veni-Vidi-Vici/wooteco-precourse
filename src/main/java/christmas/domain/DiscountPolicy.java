00e73c187d46fd85c2853a16cbe561a1bc313106
SeongIl <biuld1234@gmail.com>
2023-11-09 21:11:52 +0900

    Fix(DiscoutPolicy): fix 잘못된 계산 로직 - 테스트를 통해, 빠르게 잘못된 것을 파악할 수 있었음

--- src/main/java/christmas/domain/DiscountPolicy.java
+++ src/main/java/christmas/domain/DiscountPolicy.java
@@ -3,7 +3,7 @@ package christmas.domain;
 public class DiscountPolicy {
 
     public static Integer calculateChristmas(Integer day) {
-        return (1000 + (day * 100));
+        return (1000 + ((day - 1) * 100));
     }
 
     public static boolean checkTarget(Integer totalAmount) {
--- src/test/java/domain/DiscountPolicyTest.java
+++ src/test/java/domain/DiscountPolicyTest.java
@@ -2,23 +2,24 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.domain.DiscountPolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class DiscountTest {
+public class DiscountPolicyTest {
     @DisplayName("크리스마스 디데이 할인 금액 반환")
     @ParameterizedTest
     @CsvSource(value = {"1,1000", "25,3400"})
-    void discountChristmas(Integer day, Integer discountPrice) {
-        assertThat(discountChristmas(day)).isEqualTo(discountPrice);
+    void calculateChristmas(Integer day, Integer discountPrice) {
+        assertThat(DiscountPolicy.calculateChristmas(day)).isEqualTo(discountPrice);
     }
 
     @DisplayName("할인 대상 검증")
     @ParameterizedTest
     @CsvSource(value = {"10000,true","25000,true","9999,false","3000,false"})
-    void checkDiscountTarget(Integer totalAmount, boolean isTarget){
-        assertThat(checkDiscountTarget(totalAmount)).isEqualTo(isTarget);
+    void checkTarget(Integer totalAmount, boolean isTarget){
+        assertThat(DiscountPolicy.checkTarget(totalAmount)).isEqualTo(isTarget);
 
     }
 }
