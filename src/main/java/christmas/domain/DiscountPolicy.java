d9f6f2063012f6b40b03f8b38fd6b0b8c433cb53
SeongIl <biuld1234@gmail.com>
2023-11-09 21:08:17 +0900

    rename: Discount -> DiscountPolicy - 클래스 이름이 다소 동사처럼 보여질 수 있어서 수정

--- src/main/java/christmas/domain/DiscountPolicy.java
+++ src/main/java/christmas/domain/DiscountPolicy.java
@@ -1,6 +1,6 @@
 package christmas.domain;
 
-public class Discount {
+public class DiscountPolicy {
 
     public static Integer calculateChristmas(Integer day) {
         return (1000 + (day * 100));
