203342e819218f3daa89ca0fda0c816b261d366d
SeongIl <biuld1234@gmail.com>
2023-11-09 20:43:26 +0900

    test(DiscountTest): add 할인 대상 검증

--- src/test/java/christmas/DiscountTest.java
+++ src/test/java/christmas/DiscountTest.java
@@ -10,7 +10,15 @@ public class DiscountTest {
     @DisplayName("크리스마스 디데이 할인 금액 반환")
     @ParameterizedTest
     @CsvSource(value = {"1,1000", "25,3400"})
-    void discountChristmas(Integer day, Integer discountPrice){
+    void discountChristmas(Integer day, Integer discountPrice) {
         assertThat(discountChristmas(day)).isEqualTo(discountPrice);
+    }
+
+    @DisplayName("할인 대상 검증")
+    @ParameterizedTest
+    @CsvSource(value = {"10000,true","25000,true","9999,false","3000,false"})
+    void checkDiscountTarget(Integer totalAmount, boolean isTarget){
+        assertThat(checkDiscountTarget(totalAmount)).isEqualTo(isTarget);
+
     }
 }
