ed2d71b7d29790f7373937cef9e1a2d989a05b8a
SeongIl <biuld1234@gmail.com>
2023-11-10 20:10:49 +0900

    docs: modify 날짜 입력 예외 처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -22,15 +22,17 @@ public class InputView {
         }
     }
 
+    public static void checkZero(String userInput) {
+        if(userInput.charAt(0) == '0'){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
     public static void checkRange(Integer userInput) {
         if ((userInput < 1) || (userInput > 31)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
-    public static void checkZero(String userInput) {
-        if(userInput.charAt(0) == '0'){
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
-    }
+
 }
--- src/test/java/christmas/view/InputViewTest.java
+++ src/test/java/christmas/view/InputViewTest.java
@@ -6,13 +6,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest {
-
 
     @DisplayName("날짜 받아오기")
     @ParameterizedTest
@@ -42,14 +40,6 @@ public class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("날짜 범위를 벗어날 때, 예외 처리")
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 32})
-    void checkRange(Integer userInput) {
-        assertThatThrownBy(() -> InputView.checkRange(userInput))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("입력 값 앞에 0이 있을 때, 예외 처리")
     @ParameterizedTest
     @ValueSource(strings = {"01", "000000010", "0", "0a"})
@@ -58,4 +48,11 @@ public class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("날짜 범위를 벗어날 때, 예외 처리")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 32})
+    void checkRange(Integer userInput) {
+        assertThatThrownBy(() -> InputView.checkRange(userInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
