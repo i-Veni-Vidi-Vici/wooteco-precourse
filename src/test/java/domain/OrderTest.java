6049c1b2618867af8015f28ae057fc49368b4a1e
SeongIl <biuld1234@gmail.com>
2023-11-10 12:38:57 +0900

    test(OredrTest): add 음식 타입 개수 체크

--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -6,23 +6,53 @@ import christmas.constants.Food;
 import christmas.domain.Order;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
+    private static Map<Food, Integer> orderedMenu;
+
+    @BeforeAll
+    static void beforeAll() {
+        orderedMenu = new HashMap<>();
+
+        // 애피타이저 - 3
+        orderedMenu.put(Food.MUSHROOM_SOUP, 1);
+        orderedMenu.put(Food.CAESAR_SALAD, 2);
+        // 메인 - 4
+        orderedMenu.put(Food.T_BONE_STEAK, 2);
+        orderedMenu.put(Food.BBQ_RIBS, 1);
+        orderedMenu.put(Food.CHRISTMAS_PASTA, 1);
+        // 디저트 - 1
+        orderedMenu.put(Food.CHOCO_CAKE, 1);
+        // 음료 -3
+        orderedMenu.put(Food.ZERO_COLA, 1);
+        orderedMenu.put(Food.RED_WINE, 1);
+        orderedMenu.put(Food.CHAMPAGNE, 1);
+    }
 
     @DisplayName("주문 메뉴에서 애피타이저 개수 체크")
     @Test
     void checkAppetizer() {
-        Map<Food, Integer> orderedMenu = new HashMap<>();
-
-        orderedMenu.put(Food.MUSHROOM_SOUP, 1);
-        orderedMenu.put(Food.CAESAR_SALAD, 2);
-
-        orderedMenu.put(Food.T_BONE_STEAK, 3);
-        orderedMenu.put(Food.CHOCO_CAKE, 1);
-        orderedMenu.put(Food.ZERO_COLA, 3);
-
         assertThat(Order.checkAppetizer(orderedMenu)).isEqualTo(3);
+    }
+
+    @DisplayName("주문 메뉴에서 메인 개수 체크")
+    @Test
+    void checkMain() {
+        assertThat(Order.checkMain(orderedMenu)).isEqualTo(3);
+    }
+
+    @DisplayName("주문 메뉴에서 디저트 개수 체크")
+    @Test
+    void checkDessert() {
+        assertThat(Order.checkDessert(orderedMenu)).isEqualTo(3);
+    }
+
+    @DisplayName("주문 메뉴에서 음료 개수 체크")
+    @Test
+    void checkDrink() {
+        assertThat(Order.checkDrink(orderedMenu)).isEqualTo(3);
     }
 }
