bdad2b5fd7c84f53924ad8507cf803aa7e8688a5
SeongIl <biuld1234@gmail.com>
2023-11-10 12:15:27 +0900

    feat(Food): add 음식 종류

--- src/main/java/christmas/constants/Food.java
+++ src/main/java/christmas/constants/Food.java
@@ -1,24 +1,30 @@
 package christmas.constants;
 
 public enum Food {
-    MUSHROOM_SOUP("양송이수프", 6_000),
-    TAPAS("타파스", 5_500),
-    CAESAR_SALAD("시저샐러드", 8_000),
-    T_BONE_STEAK("티본스테이크", 55_000),
-    BBQ_RIBS("바비큐립", 54_000),
-    SEAFOOD_PASTA("해산물파스타", 35_000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
-    CHOCO_CAKE("초코케이크", 15_000),
-    ICE_CREAM("아이스크림", 5_000),
-    ZERO_COLA("제로콜라", 3_000),
-    RED_WINE("레드와인", 60_000),
-    CHAMPAGNE("샴페인", 25_000);
+    MUSHROOM_SOUP("양송이수프", "애피타이저", 6_000),
+    TAPAS("타파스", "애피타이저", 5_500),
+    CAESAR_SALAD("시저샐러드", "애피타이저", 8_000),
+
+    T_BONE_STEAK("티본스테이크", "메인", 55_000),
+    BBQ_RIBS("바비큐립", "메인", 54_000),
+    SEAFOOD_PASTA("해산물파스타", "메인", 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", "메인", 25_000),
+
+    CHOCO_CAKE("초코케이크", "디저트", 15_000),
+    ICE_CREAM("아이스크림", "디저트", 5_000),
+
+    ZERO_COLA("제로콜라", "음료", 3_000),
+    RED_WINE("레드와인", "음료", 60_000),
+    CHAMPAGNE("샴페인", "음료", 25_000);
 
     private final String name;
+    private final String type;
+
     private final int price;
 
-    Food(String name, int price) {
+    Food(String name, String type, int price) {
         this.name = name;
+        this.type = type;
         this.price = price;
     }
 
@@ -30,7 +36,9 @@ public enum Food {
         return price;
     }
 
-
+    public String getType() {
+        return type;
+    }
 }
 
 
--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,8 +1,13 @@
 package christmas.domain;
 
+import christmas.constants.Food;
+import java.util.Map;
+
 public class Order {
 
     public static Integer checkAppetizer(Map<Food, Integer> orderedMenu) {
+
+
 
     }
 }
