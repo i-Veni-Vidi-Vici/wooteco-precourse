fc18902444fc6cee3091996fb1dd813116029016
SeongIl <biuld1234@gmail.com>
2023-11-12 22:45:59 +0900

    feat(EventPlanner): add 날짜 예약 메서드

--- src/main/java/christmas/EventPlanner.java
+++ src/main/java/christmas/EventPlanner.java
@@ -1,11 +1,26 @@
 package christmas;
 
+import christmas.domain.ReservedDate;
+import christmas.utility.Converter;
+import christmas.view.InputView;
+
 public class EventPlanner {
 
 
-    public void reserveDate{
+    public void reserveDate() {
+        boolean isReserving = true;
 
+        while (isReserving) {
+            try {
+                ReservedDate reservedDate = new ReservedDate(Converter.convertToNumber(InputView.getDate()));
+                isReserving = false;
+            } catch (IllegalArgumentException ex) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
     }
+
+
 
 
 }
