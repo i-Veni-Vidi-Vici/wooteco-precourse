890d4d4cf95b1a1c974b59a8590b2215d0f53e3c
SeongIl <biuld1234@gmail.com>
2023-11-13 23:45:03 +0900

    refactor(Value): 오름차순으로 정렬

--- src/main/java/christmas/constants/Value.java
+++ src/main/java/christmas/constants/Value.java
@@ -1,72 +1,53 @@
 package christmas.constants;
 
 public enum Value {
-
-    SPECIAL_DISCOUNT(1000),
     ZERO(0),
+    FIRST_CHARACTER(0),
     INITIAL_ZERO(0),
+    FIRST_INDEX(0),
+
+    ONE_DATE(1),
+    MIN_FOOD_COUNT(1),
+    MIN_DATE(1),
+    FIRST_WEEKEND_DATE(1),
+    ONE_LENGTH(1),
+    SECOND_INDEX(1),
+
+    FORM_SIZE(2),
+    FIRST_WEEK_LAST_WEEKEND_DATE(2),
+
+    FIRST_SPECIAL_DATE(3),
+    FIRST_WEEKDAY_DATE(3),
 
     FIVE_WEEKS(5),
 
-    FIRST_SPECIAL_DATE(3),
-
-    FIRST_WEEKDAY_DATE(3),
-
+    SEVEN_DAYS(7),
     FIRST_WEEK_LAST_WEEKDAY_DATE(7),
 
-    SEVEN_DAYS(7),
 
+    MAX_TOTAL_FOOD_COUNT(20),
     XMAS_DATE(25),
+    MAX_DATE(31),
+    ASCII_ZERO(48),
+    XMAS_INCREMENT_DISCOUNT(100),
+
+    SPECIAL_DISCOUNT(1000),
+    FIRST_XMAS_DISCOUNT(1000),
 
     WEEKDAY_DISCOUNT(2023),
     WEEKEND_DISCOUNT(2023),
-    FIRST_WEEKEND_DATE(1),
-    FIRST_WEEK_LAST_WEEKEND_DATE(2),
 
-    FIRST_XMAS_DISCOUNT(1000),
-
-    ONE_DATE(1),
-
-    XMAS_INCREMENT_DISCOUNT(100),
-
-    CHAMPAGNE_PRICE(25000),
-
-    GIVEAWAY_MIN_AMOUNT_CONDITION(120000),
     BENEFIT_MIN_AMOUNT_CONDITION(10000),
+    CHAMPAGNE_PRICE(25000),
+    GIVEAWAY_MIN_AMOUNT_CONDITION(120000);
 
-    MIN_DATE(1),
-
-    MAX_DATE(31),
-
-    MIN_FOOD_COUNT(1),
-
-    MAX_TOTAL_FOOD_COUNT(20),
-
-    FIRST_CHARACTER(0),
-
-    ASCII_ZERO(48),
-
-    ONE_LENGTH(1),
-
-    FIRST_INDEX(0),
-
-    SECOND_INDEX(1),
-    FORM_SIZE(2)
-
-
-    ;
-
-
-
+    private final Integer value;
 
     Value(Integer value) {
         this.value = value;
     }
 
-    private final Integer value;
-
     public Integer get() {
         return value;
     }
-
 }
