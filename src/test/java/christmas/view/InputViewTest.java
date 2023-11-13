9b4f6f7cb03c7e03303539394685f213a97404cd
SeongIl <biuld1234@gmail.com>
2023-11-13 21:19:55 +0900

    test(InputViewTest): modify static

--- src/test/java/christmas/view/InputViewTest.java
+++ src/test/java/christmas/view/InputViewTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,14 +12,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputViewTest {
 
+    private static InputView inputView;
+
+    @BeforeAll
+    static void beforeAll() {
+        inputView = new InputView();
+    }
+
     @DisplayName("날짜 입력 받기")
     @ParameterizedTest
     @ValueSource(strings = {"1", "20", "31"})
     void getDate(String userInput) {
+        // given
+
         System.setIn(new ByteArrayInputStream(userInput.getBytes()));
 
         // when, then
-        assertThat(InputView.getDate()).isEqualTo(userInput);
+        assertThat(inputView.getDate()).isEqualTo(userInput);
 
         Console.close();
     }
@@ -26,11 +36,10 @@ public class InputViewTest {
     @DisplayName("주문 메뉴 입력 받기")
     @Test
     void getOrder() {
-        // given
         System.setIn(new ByteArrayInputStream("해산물파스타-2,레드와인-1".getBytes()));
 
         // when, then
-        assertThat(InputView.getMenu()).isEqualTo("해산물파스타-2,레드와인-1");
+        assertThat(inputView.getMenu()).isEqualTo("해산물파스타-2,레드와인-1");
 
         Console.close();
     }
