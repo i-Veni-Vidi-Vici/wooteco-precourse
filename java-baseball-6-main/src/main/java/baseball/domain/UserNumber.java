ae487e80b27761141f0fbbe042bd43954c1b53b9
SeongIl <biuld1234@gmail.com>
2023-11-20 20:05:44 +0900

    feat(UserNumber): create 범위 체크

--- java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
+++ java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
@@ -1,2 +1,14 @@
-package baseball.domain;public class UserNumber {
+package baseball.domain;
+
+import java.util.List;
+
+public class UserNumber {
+
+    public void checkRange(List<Integer> userNumbers){
+        for (Integer userNumber : userNumbers) {
+            if((userNumber < 1) || (userNumber > 9)){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
--- java-baseball-6-main/src/test/java/baseball/domain/UserNumberTest.java
+++ java-baseball-6-main/src/test/java/baseball/domain/UserNumberTest.java
@@ -1,8 +1,8 @@
 package baseball.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +11,11 @@ public class UserNumberTest {
     @DisplayName("1~9 범위를 벗어날 때, 예외 처리")
     @Test
     void checkRange(){
-        assertThatThrownBy(() -> new UserNumber())
+        UserNumber userNumber = new UserNumber();
+
+        assertThatThrownBy(() -> userNumber.checkRange(List.of(1,2,10)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> userNumber.checkRange(List.of(0,1,2)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
