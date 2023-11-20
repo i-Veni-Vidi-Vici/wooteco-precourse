53faf44aae73b6aea3264cdf667dd5be29fecc00
SeongIl <biuld1234@gmail.com>
2023-11-20 20:18:16 +0900

    feat(UserNumber): create 중복 검사

--- java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
+++ java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UserNumber {
 
@@ -9,6 +11,14 @@ public class UserNumber {
             if((userNumber < 1) || (userNumber > 9)){
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    public void checkDuplication(List<Integer> userNumbers) {
+        Set<Integer> differentNumbers = new HashSet<>(userNumbers);
+
+        if (userNumbers.size() != differentNumbers.size()) {
+            throw new IllegalArgumentException();
         }
     }
 }
