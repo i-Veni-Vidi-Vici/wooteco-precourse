ce944dc4b5fc3f88ae9795bec1e3b355b07fa62e
SeongIl <biuld1234@gmail.com>
2023-11-20 20:34:32 +0900

    refactor(UserNumber): add 생성자+상태

--- java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
+++ java-baseball-6-main/src/main/java/baseball/domain/UserNumber.java
@@ -1,12 +1,21 @@
 package baseball.domain;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class UserNumber {
 
-    public void checkRange(List<Integer> userNumbers){
+    private final List<Integer> userNumber;
+
+    public UserNumber(List<Integer> userNumber) {
+        checkRange(userNumber);
+        checkDuplication(userNumber);
+        this.userNumber = userNumber;
+    }
+
+    private void checkRange(List<Integer> userNumbers){
         for (Integer userNumber : userNumbers) {
             if((userNumber < 1) || (userNumber > 9)){
                 throw new IllegalArgumentException();
@@ -14,11 +23,15 @@ public class UserNumber {
         }
     }
 
-    public void checkDuplication(List<Integer> userNumbers) {
+    private void checkDuplication(List<Integer> userNumbers) {
         Set<Integer> differentNumbers = new HashSet<>(userNumbers);
 
         if (userNumbers.size() != differentNumbers.size()) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public List<Integer> get() {
+        return Collections.unmodifiableList(userNumber);
     }
 }
--- java-baseball-6-main/src/test/java/baseball/domain/UserNumberTest.java
+++ java-baseball-6-main/src/test/java/baseball/domain/UserNumberTest.java
@@ -11,23 +11,18 @@ public class UserNumberTest {
     @DisplayName("1~9 범위를 벗어날 때, 예외 처리")
     @Test
     void checkRange(){
-        UserNumber userNumber = new UserNumber();
-
-        assertThatThrownBy(() -> userNumber.checkRange(List.of(1,2,10)))
+        assertThatThrownBy(() -> new UserNumber(List.of(1,2,10)))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> userNumber.checkRange(List.of(0,1,2)))
+        assertThatThrownBy(() -> new UserNumber(List.of(0,1,2)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("중복된 숫자가 있을 때, 예외 처리")
     @Test
     void checkDuplication(){
-
-        UserNumber userNumber = new UserNumber();
-
-        assertThatThrownBy(() -> userNumber.checkDuplication(List.of(1, 1, 2)))
+        assertThatThrownBy(() -> new UserNumber(List.of(1, 1, 2)))
                 .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> userNumber.checkDuplication(List.of(3, 5, 5)))
+        assertThatThrownBy(() -> new UserNumber(List.of(3, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
