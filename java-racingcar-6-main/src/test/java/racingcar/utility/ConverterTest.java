b3342fbe1dde290741b6e0a319b2a1b28f546b84
SeongIl <biuld1234@gmail.com>
2023-11-21 16:21:21 +0900

    test(ConverterTest): create 변환 테스트

---
+++ java-racingcar-6-main/src/test/java/racingcar/utility/ConverterTest.java
@@ -0,0 +1,69 @@
+package racingcar.utility;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ConverterTest {
+
+    @DisplayName("문자열을 Map으로 변환")
+    @Test
+    void convertToMap() {
+        Converter converter = new Converter();
+        assertThat(converter.convertToMap("pobi,woni,jun"))
+                .isEqualTo(Map.of("pobi", "", "woni", "", "jun", ""));
+    }
+
+    @DisplayName("앞 뒤 공백 있을 때, 예외 처리")
+    @ParameterizedTest
+    @ValueSource(strings = {" pobi,a", "woni ,a", "a, jun "})
+    void convertToMapByWhitespace(String value) {
+        Converter converter = new Converter();
+
+        assertThatThrownBy(() -> converter.convertToMap(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복된 문자열일 때, 예외 처리")
+    @Test
+    void convertToMapByDuplication() {
+        Converter converter = new Converter();
+
+        assertThatThrownBy(() -> converter.convertToMap("pobi,pobi"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문자열을 Integer로 변환")
+    @Test
+    void convertToNumber() {
+        Converter converter = new Converter();
+        assertThat(converter.convertToNumber("3")).isEqualTo(3);
+    }
+
+    @DisplayName("정수가 아닐 때, 예외 처리")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "", "a", "1.0"})
+    void convertToNumberByNotInteger(String value) {
+        Converter converter = new Converter();
+
+        assertThatThrownBy(() -> converter.convertToNumber(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 앞에 0이 포함될 때(2자리 이상), 예외 처리")
+    @ParameterizedTest
+    @ValueSource(strings = {"01", "00", "000005"})
+    void convertToNumberByFirstZero(String value) {
+        Converter converter = new Converter();
+
+        assertThatThrownBy(() -> converter.convertToNumber(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
