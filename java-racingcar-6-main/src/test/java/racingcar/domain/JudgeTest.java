baea9df9788ad4dc83e17d43d00dd20f5ce3ecc4
SeongIl <biuld1234@gmail.com>
2023-11-21 17:58:10 +0900

    test(JudgeTest): create 심사위원

---
+++ java-racingcar-6-main/src/test/java/racingcar/domain/JudgeTest.java
@@ -0,0 +1,32 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JudgeTest {
+
+    @DisplayName("우승자 선별, 다수")
+    @Test
+    void selectWinnerByMany() {
+        Judge judge = new Judge();
+
+        Map<String, String> car = new LinkedHashMap<>(Map.of("pobi", "-",
+                "woni", "", "jun", "-"));
+        assertThat(judge.selectWinner(car)).isEqualTo(List.of("pobi","jun"));
+    }
+
+    @DisplayName("우승자 선별, 한명")
+    @Test
+    void selectWinnerByOne() {
+        Judge judge = new Judge();
+
+        Map<String, String> car = new LinkedHashMap<>(Map.of("pobi", "---",
+                "woni", "--", "jun", "-"));
+        assertThat(judge.selectWinner(car)).isEqualTo(List.of("pobi"));
+    }
+}
