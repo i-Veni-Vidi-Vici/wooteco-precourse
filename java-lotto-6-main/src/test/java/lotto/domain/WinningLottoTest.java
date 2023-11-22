a1fd8626f97e029ebfa305af19e53394130d0cea
SeongIl <biuld1234@gmail.com>
2023-11-22 18:23:22 +0900

    test(WinningLottoTset): add 보너스 번호가 당첨번호와 중복될 때, 예외 처리

--- java-lotto-6-main/src/test/java/lotto/domain/WinningLottoTest.java
+++ java-lotto-6-main/src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -14,5 +15,12 @@ public class WinningLottoTest {
         WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
         assertThat(winningLotto.getWinningNumber().getNumbers()).isEqualTo(List.of(1,2,3,4,5,6));
         assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
+    }
+
+    @DisplayName("보너스 번호가 당첨번호와 중복될 때, 예외 처리")
+    @Test
+    void createByDuplication(){
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 6))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
