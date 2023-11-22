d7c0e8906ff37b65f4a8c684daecd300d662c8da
SeongIl <biuld1234@gmail.com>
2023-11-22 20:22:05 +0900

    feat(OutputView): create OutputView

--- java-lotto-6-main/src/main/java/lotto/constants/Prize.java
+++ java-lotto-6-main/src/main/java/lotto/constants/Prize.java
@@ -1,12 +1,11 @@
 package lotto.constants;
 
 public enum Prize {
-
-    FIRST(6, "6개 일치", 2_000_000_000),
-    SECOND(5, "5개 일치, 보너스 볼 일치", 30_000_000),
-    THIRD(5, "5개 일치", 1_500_000),
+    FIFTH(3, "3개 일치", 5_000),
     FOURTH(4, "4개 일치", 50_000),
-    FIFTH(3, "3개 일치", 5_000);
+    THIRD(5, "5개 일치", 1_500_000),
+    SECOND(5, "5개 일치, 보너스 볼 일치", 30_000_000),
+    FIRST(6, "6개 일치", 2_000_000_000);
 
 
     private final Integer matchingCount;
@@ -25,7 +24,7 @@ public enum Prize {
             return FIRST;
         }
         if (matchingCount == 5) {
-           return calculateSecond(bonusNumber);
+            return calculateSecond(bonusNumber);
         }
         if (matchingCount == 4) {
             return FOURTH;
---
+++ java-lotto-6-main/src/test/java/lotto/view/OutputView.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import lotto.constants.Prize;
+import lotto.domain.Lotto;
+
+public class OutputView {
+
+
+    public void printLotto(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+
+        for (Lotto lotto : lottos) {
+            List<Integer> sortedLotto = new ArrayList<>(lotto.getNumbers());
+            Collections.sort(sortedLotto);
+            System.out.println(sortedLotto);
+        }
+    }
+
+    public void printResult(Map<Prize, Integer> lottoResult) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        for (Prize prize : Prize.values()) {
+            System.out.println(prize.getMessage() + " "
+                    + "(" + String.format("%,d", prize.getPrizeMoney()) + "원"+")" + " " + "-" + " "
+                    + lottoResult.get(prize) + "개");
+        }
+    }
+
+    public void printEarningsRate(Double earningsRate) {
+        System.out.println("총 수익률은 " + String.format("%,.1f",earningsRate) +"%입니다.");
+
+    }
+}
