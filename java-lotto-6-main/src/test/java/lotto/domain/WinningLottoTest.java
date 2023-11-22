0f725136bb42e4da809bfd38ca0b1d7396428b1d
SeongIl <biuld1234@gmail.com>
2023-11-22 18:09:24 +0900

    test(WinningLotto): create 당첨 로또 생성

---
+++ java-lotto-6-main/src/test/java/lotto/domain/WinningLottoTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WinningLottoTest {
+
+    @DisplayName("당첨 로또 생성")
+    @Test
+    void create(){
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1,2,3,4,5,6)), 7);
+        assertThat(winningLotto.getWinningLotto()).isEqualTo(List.of(1,2,3,4,5,6));
+        assertThat(winningLotto.getBonusNumber()).isEqualTo(7);
+    }
+}
