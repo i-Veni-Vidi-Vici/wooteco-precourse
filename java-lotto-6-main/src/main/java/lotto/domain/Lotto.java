3f4c5842b30b06d5bf6638e9524c24227cd1a4a7
SeongIl <biuld1234@gmail.com>
2023-11-22 17:58:35 +0900

    feat(Lotto): add 에외 처리

--- java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
+++ java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         checkSize(numbers);
+        checkDuplication(numbers);
+        checkRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,9 +20,22 @@ public class Lotto {
         }
     }
 
+    private void checkDuplication(List<Integer> numbers) {
+        Set<Integer> differentNumbers = new HashSet<>(numbers);
+        if (numbers.size() != differentNumbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if ((number < 1) || (number > 45)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
-
 }
