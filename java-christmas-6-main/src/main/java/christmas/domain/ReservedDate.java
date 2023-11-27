ff4df34d373a32f2fa79f9b81631777389b316f8
SeongIl <biuld1234@gmail.com>
2023-11-27 14:41:58 +0900

    feat(ReservedDate): add 범위를 벗어날 때, 예외 처리

--- java-christmas-6-main/src/main/java/christmas/domain/ReservedDate.java
+++ java-christmas-6-main/src/main/java/christmas/domain/ReservedDate.java
@@ -5,7 +5,14 @@ public class ReservedDate {
     private final Integer reservedDate;
 
     public ReservedDate(Integer reservedDate) {
+        checkRange(reservedDate);
         this.reservedDate = reservedDate;
+    }
+
+    private void checkRange(Integer reservedDate) {
+        if ((reservedDate < 1) || (reservedDate > 31)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public Integer get() {
