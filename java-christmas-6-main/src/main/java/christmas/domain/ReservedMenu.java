10db41ed06a2fd71f0f093c0a702f923cb19536d
SeongIl <biuld1234@gmail.com>
2023-11-27 15:07:26 +0900

    feat(ReservedMenu): add 메뉴 주문 에외 처리

--- java-christmas-6-main/src/main/java/christmas/domain/ReservedMenu.java
+++ java-christmas-6-main/src/main/java/christmas/domain/ReservedMenu.java
@@ -10,9 +10,32 @@ public class ReservedMenu {
     private final Map<Food, Integer> reservedMenu;
 
     public ReservedMenu(Map<Food, Integer> reservedMenu) {
+        checkOnlyDrink(reservedMenu);
+        checkTotalCount(reservedMenu);
+        checkMenuCount(reservedMenu);
+
         this.reservedMenu = reservedMenu;
     }
 
+    private void checkOnlyDrink(Map<Food, Integer> reservedMenu) {
+        if (countDrink(reservedMenu).equals(countMenu(reservedMenu))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkTotalCount(Map<Food, Integer> reservedMenu) {
+        if (countMenu(reservedMenu) > 20) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkMenuCount(Map<Food, Integer> reservedMenu) {
+        for (Food food : reservedMenu.keySet()) {
+            if (reservedMenu.get(food) < 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
     public Integer calculateAmount() {
         Integer sum = 0;
@@ -29,7 +52,7 @@ public class ReservedMenu {
 
         for (Food food : reservedMenu.keySet()) {
             if (food.getType() == Type.DESSERT) {
-                dessertCount++;
+                dessertCount += reservedMenu.get(food);
             }
         }
 
@@ -41,11 +64,33 @@ public class ReservedMenu {
 
         for (Food food : reservedMenu.keySet()) {
             if (food.getType() == Type.MAIN) {
-                mainCount++;
+                mainCount += reservedMenu.get(food);
             }
         }
 
         return mainCount;
+    }
+
+    private Integer countDrink(Map<Food, Integer> reservedMenu) {
+        Integer drinkCount = 0;
+
+        for (Food food : reservedMenu.keySet()) {
+            if (food.getType() == Type.DRINK) {
+                drinkCount += reservedMenu.get(food);
+            }
+        }
+
+        return drinkCount;
+    }
+
+    private Integer countMenu(Map<Food, Integer> reservedMenu) {
+        Integer menuCount = 0;
+
+        for (Food food : reservedMenu.keySet()) {
+            menuCount += reservedMenu.get(food);
+        }
+
+        return menuCount;
     }
 
 
