5748ea5958df21e9370c3822d2e1e6ef7b428c62
SeongIl <biuld1234@gmail.com>
2023-11-29 20:04:03 +0900

    refactor(NickName): separate 메서드

--- 5/java-onboarding-main/src/main/java/onboarding/Nickname.java
+++ 5/java-onboarding-main/src/main/java/onboarding/Nickname.java
@@ -9,40 +9,49 @@ import java.util.Set;
 public class Nickname {
     public List<String> checkDuplication(List<List<String>> nicknames) {
         Set<String> emails = new HashSet<>();
-        Set<String> substringNicknames;
 
         for (int index = 0; index < nicknames.size(); index++) {
             String nickname = nicknames.get(index).get(1);
-            String email = nicknames.get(index).get(0);
-            substringNicknames = new HashSet<>();
-
+            Set<String> substringNicknames = new HashSet<>();
             createSeparationNickname(substringNicknames, nickname, nickname.length());
+            List<List<String>> otherNicknames = nicknames.subList(index + 1, nicknames.size());
 
-            for (int otherIndex = index + 1; otherIndex < nicknames.size(); otherIndex++) {
-                String otherNickname = nicknames.get(otherIndex).get(1);
-                String otherEmail = nicknames.get(otherIndex).get(0);
-
-                for (String substringNickname : substringNicknames) {
-                    if (otherNickname.contains(substringNickname)) {
-                        emails.add(email);
-                        emails.add(otherEmail);
-                    }
-                }
-
+            if (findDuplication(substringNicknames, emails, otherNicknames)) {
+                emails.add(nicknames.get(index).get(0));
             }
         }
 
+        return sortEmails(emails);
+    }
+
+    private List<String> sortEmails(Set<String> emails) {
         List<String> sortedEmails = new ArrayList<>(emails);
         Collections.sort(sortedEmails);
         return sortedEmails;
     }
 
-    private void createSeparationNickname(Set<String> substringNicknames, String nickname, Integer nicknameLength){
+    private void createSeparationNickname(Set<String> substringNicknames, String nickname, Integer nicknameLength) {
         for (int startIndex = 0; startIndex < nicknameLength - 1; startIndex++) {
             for (int endIndex = startIndex + 2; endIndex < nicknameLength + 1; endIndex++) {
                 substringNicknames.add(nickname.substring(startIndex, endIndex));
 
             }
         }
+    }
+
+    private boolean findDuplication(Set<String> substringNicknames, Set<String> emails,
+                                    List<List<String>> otherNicknames) {
+        boolean isDuplication = false;
+
+        for (List<String> otherNickname : otherNicknames) {
+            for (String substringNickname : substringNicknames) {
+                if (otherNickname.get(1).contains(substringNickname)) {
+                    emails.add(otherNickname.get(0));
+                    isDuplication = true;
+                }
+            }
+        }
+
+        return isDuplication;
     }
 }
