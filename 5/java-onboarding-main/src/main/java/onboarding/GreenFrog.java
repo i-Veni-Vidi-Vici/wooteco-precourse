5902b06ff1a1f110cc8dc9e86be7e05d7b84df01
SeongIl <biuld1234@gmail.com>
2023-11-29 16:56:38 +0900

    feat(GreenFrog): create 문자열 뒤집기

---
+++ 5/java-onboarding-main/src/main/java/onboarding/GreenFrog.java
@@ -0,0 +1,11 @@
+package onboarding;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class GreenFrog {
+    public String reverse(String value) {
+        return new StringBuilder(value).reverse().toString();
+    }
+}
