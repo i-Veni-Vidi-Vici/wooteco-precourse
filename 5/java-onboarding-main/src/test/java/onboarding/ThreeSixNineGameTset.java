6f5cc966b4421261b3e2a2149dac42cae267079e
SeongIl <biuld1234@gmail.com>
2023-11-29 16:42:27 +0900

    feat(ThreeSixNineGame): create 짝 계산

--- 5/java-onboarding-main/src/test/java/onboarding/ThreeSixNineGameTset.java
+++ 5/java-onboarding-main/src/test/java/onboarding/ThreeSixNineGameTset.java
@@ -3,10 +3,8 @@ package onboarding;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class ThreeSixNineGameTset {
 
@@ -14,6 +12,8 @@ public class ThreeSixNineGameTset {
     @ParameterizedTest
     @CsvSource(value = {"13,4", "33,14"})
     void calculateClap(Integer number, Integer clapCount) {
-        assertThat(calculateClap(number)).isEqualTo(clapCount);
+        ThreeSixNineGame threeSixNineGame = new ThreeSixNineGame();
+
+        assertThat(threeSixNineGame.calculateClap(number)).isEqualTo(clapCount);
     }
 }
