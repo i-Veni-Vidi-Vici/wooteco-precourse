7e25e34c7834e4d28b8cf2273f4fbee53287a3c6
SeongIl <biuld1234@gmail.com>
2023-11-30 16:43:35 +0900

    feat(OutputView): create 출력

--- 5/java-bridge-main/src/main/java/bridge/view/OutputView.java
+++ 5/java-bridge-main/src/main/java/bridge/view/OutputView.java
@@ -1,23 +1,60 @@
 package bridge.view;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
-public class OutputView {
+import java.util.List;
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printMap() {
+public class OutputView {
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     * <p>
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
-    public void printResult() {
+    public void printMap(List<List<String>> result) {
+        printOneMap(result, "U");
+        printOneMap(result, "D");
+    }
+
+    private void printOneMap(List<List<String>> result, String direction) {
+        StringBuilder oneMap = new StringBuilder("[");
+
+        for (List<String> stage : result) {
+            if (oneMap.length() > 1) {
+                oneMap.append("|");
+            }
+
+            oneMap.append(expressMap(stage, direction));
+        }
+
+        oneMap.append("]");
+        System.out.println(oneMap);
+    }
+
+    private String expressMap(List<String> stage, String direction) {
+        if (stage.get(0).equals(direction)) {
+            return (" " + stage.get(1) + " ");
+
+        }
+
+        return (" " + " " + " ");
+    }
+
+    public void printResult(List<List<String>> result, Integer retryCount) {
+        System.out.println("최종 게임 결과");
+        printMap(result);
+
+        System.out.println("게임 성공 여부: " + calculateResult(result));
+        System.out.println("총 시도한 횟수: " + retryCount);
+    }
+
+    private String calculateResult(List<List<String>> result) {
+        for (List<String> stage : result) {
+            if (stage.contains("X")) {
+                return "실패";
+            }
+        }
+
+        return "성공";
+    }
+
+    public void printError(String error) {
+        System.out.println(error);
     }
 }
