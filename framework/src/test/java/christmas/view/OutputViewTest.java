2834589a77e15a45b30d82b6af519f2223323a61
SeongIl <biuld1234@gmail.com>
2023-12-02 16:41:04 +0900

    i

--- framework/src/test/java/christmas/view/OutputViewTest.java
+++ framework/src/test/java/christmas/view/OutputViewTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 public class OutputViewTest {
 
     private static OutputView outputView;
