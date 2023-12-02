e778de86f2d8ee24a3dead8fdb57b1fb37bb15b2
SeongIl <biuld1234@gmail.com>
2023-12-02 16:38:32 +0900

    test(InputViewTest): create 입력

---
+++ 5/java-menu-main/src/test/java/menu/view/InputViewTest.java
@@ -0,0 +1,41 @@
+package menu.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+
+    private static InputView inputView;
+
+    @BeforeAll
+    static void beforeAll() {
+        inputView = new InputView();
+    }
+
+    @AfterEach
+    void afterEach() {
+        Console.close();
+    }
+
+    @DisplayName("코치 이름 입력")
+    @Test
+    void getCoaches() {
+        System.setIn(new ByteArrayInputStream("abc,ab".getBytes()));
+
+        assertThat(inputView.getCoaches()).isEqualTo("abc,ab");
+    }
+
+    @DisplayName("각 코치들이 못 먹는 메뉴 입력")
+    @Test
+    void getInedibleMenu() {
+        System.setIn(new ByteArrayInputStream("규동,우동".getBytes()));
+
+        assertThat(inputView.getInedibleMenu()).isEqualTo("규동,우동");
+    }
+}
