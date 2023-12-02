0b35d2fb265c3c7c2ba406826801460f0114af1f
SeongIl <biuld1234@gmail.com>
2023-12-02 16:40:24 +0900

    feat(InputView): craete 입력

--- 5/java-menu-main/src/test/java/menu/view/InputViewTest.java
+++ 5/java-menu-main/src/test/java/menu/view/InputViewTest.java
@@ -2,9 +2,7 @@ package menu.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.io.ByteArrayInputStream;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,11 +14,6 @@ public class InputViewTest {
     @BeforeAll
     static void beforeAll() {
         inputView = new InputView();
-    }
-
-    @AfterEach
-    void afterEach() {
-        Console.close();
     }
 
     @DisplayName("코치 이름 입력")
