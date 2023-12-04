991a5496bca61fe9947763b4a8d07578af70c4aa
SeongIl <biuld1234@gmail.com>
2023-12-04 16:35:04 +0900

    test(CoinTest): create 동전 개수 계산

---
+++ 4/java-vendingmachine-precourse-main/src/test/java/vendingmachine/constants/CoinTest.java
@@ -0,0 +1,17 @@
+package vendingmachine.constants;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CoinTest {
+
+    @DisplayName("동전 개수 계산")
+    @Test
+    void calculate(){
+        assertThat(Coin.calculate(450)).isEqualTo(Arrays.asList(0,4,1,0));
+    }
+
+}
