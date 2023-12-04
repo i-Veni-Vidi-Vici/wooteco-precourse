8c8f5b42a131323692476c55dd3c203a708c8a60
SeongIl <biuld1234@gmail.com>
2023-12-04 19:11:20 +0900

    feat(OutputView): create 출력

---
+++ 4/java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -0,0 +1,31 @@
+package vendingmachine.view;
+
+
+import java.util.Map;
+
+public class OutputView {
+
+    public void printError(String error) {
+        System.out.println(error);
+    }
+
+    public void printCoin(Map<String, Integer> coins) {
+        System.out.println("자판기가 보유한 동전");
+
+        for (String coin : coins.keySet()) {
+            System.out.println(coin + " - " + coins.get(coin) + "개");
+        }
+    }
+
+    public void printMoney(Integer money) {
+        System.out.println("투입 금액: " + money + "원");
+    }
+
+    public void printChange(Map<String, Integer> change) {
+        System.out.println("잔돈");
+
+        for (String coin : change.keySet()) {
+            System.out.println(coin + " - " + change.get(coin) + "개");
+        }
+    }
+}
