8811b98df94e3ced8234be7d518c07490bd2a1b4
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-12-08 18:44:08 +0900

    chore: git 관리대상 제외

--- 3/java-subway-map-precourse-main/src/main/java/view/InputView.java
+++ 3/java-subway-map-precourse-main/src/main/java/view/InputView.java
@@ -1,11 +1,14 @@
-package christmas.view;
+package view;
 
-import camp.nextstep.edu.missionutils.Console;
+
+import java.io.Console;
+import java.util.Scanner;
 
 public class InputView {
-    public String getDate() {
-        System.out.println("");
-        return Console.readLine();
+    public String getFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        Console console = new Scanner(System.in);
+        return
     }
 
     public String getMenu() {
