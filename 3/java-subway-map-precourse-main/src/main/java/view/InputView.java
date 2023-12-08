1066e9914febab6cbc2dc9438b0b4add8c421085
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2023-12-08 18:41:21 +0900

    i;

---
+++ 3/java-subway-map-precourse-main/src/main/java/view/InputView.java
@@ -0,0 +1,15 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getDate() {
+        System.out.println("");
+        return Console.readLine();
+    }
+
+    public String getMenu() {
+        System.out.println("");
+        return Console.readLine();
+    }
+}
